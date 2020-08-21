72b015e697f83043412c4828991acc7a0766c028
ComaszTyrulik <tomaszcyrulikdevelopment@gmail.com>
2020-08-21 17:09:33 +0200

    Applied clang-format to macros

--- include/BeastEngine/Assertions.h
+++ include/BeastEngine/Assertions.h
@@ -4,35 +4,35 @@
 namespace be::internals
 {
 #ifndef BE_DEBUG_BREAK
-#    ifdef _MSC_VER
-#        define BE_DEBUG_BREAK() DebugBreak()
-#    endif
+    #ifdef _MSC_VER
+        #define BE_DEBUG_BREAK() DebugBreak()
+    #endif
 #endif
 
 #ifndef BE_CRITICAL_ASSERT
-#    ifdef BE_ASSERTIONS_ENABLED
-#        define BE_CRITICAL_ASSERT(expression)                                                \
+    #ifdef BE_ASSERTIONS_ENABLED
+        #define BE_CRITICAL_ASSERT(expression)                                                \
             if (expression) {}                                                                \
             else                                                                              \
             {                                                                                 \
                 BE_DEBUG_LOG_FATAL_ERROR("The critical '{}' assertion failed!", #expression); \
                 BE_DEBUG_BREAK();                                                             \
             }
-#    else
-#        define BE_CRITICAL_ASSERT(expression)
-#    endif
+    #else
+        #define BE_CRITICAL_ASSERT(expression)
+    #endif
 #endif
 
 #ifndef BE_ASSERT
-#    ifdef BE_ASSERTIONS_ENABLED
-#        define BE_ASSERT(expression)                                          \
+    #ifdef BE_ASSERTIONS_ENABLED
+        #define BE_ASSERT(expression)                                          \
             if (expression) {}                                                 \
             else                                                               \
             {                                                                  \
                 BE_DEBUG_LOG_ERROR("The '{}' assertion failed!", #expression); \
             }
-#    else
-#        define BE_ASSERT(expression)
-#    endif
+    #else
+        #define BE_ASSERT(expression)
+    #endif
 #endif
 }; // namespace be::internals
--- include/BeastEngine/Definitions/Helpers.h
+++ include/BeastEngine/Definitions/Helpers.h
@@ -3,25 +3,25 @@
 namespace be::internals
 {
 #ifndef CLASS_CONSTRUCTORS_IMPLEMENTATION
-#    define CLASS_CONSTRUCTORS_IMPLEMENTATION(ClassName, definition) \
-        ClassName(const ClassName&) = definition;                   \
-        ClassName(ClassName&&) = definition;                        \
-        const ClassName& operator=(const ClassName&) = definition;  \
+    #define CLASS_CONSTRUCTORS_IMPLEMENTATION(ClassName, definition) \
+        ClassName(const ClassName&) = definition;                    \
+        ClassName(ClassName&&) = definition;                         \
+        const ClassName& operator=(const ClassName&) = definition;   \
         ClassName&& operator=(ClassName&&) = definition;
 #endif
 
 #ifndef IMPLEMENT_CONSTRUCTORS_DELETED
-#    define IMPLEMENT_CONSTRUCTORS_DELETED(ClassName) CLASS_CONSTRUCTORS_IMPLEMENTATION(ClassName, delete)
+    #define IMPLEMENT_CONSTRUCTORS_DELETED(ClassName) CLASS_CONSTRUCTORS_IMPLEMENTATION(ClassName, delete)
 #endif
 
 #ifndef IMPLEMENT_CLASS_NOT_CONSTRUCTIBLE
-#    define IMPLEMENT_CLASS_NOT_CONSTRUCTIBLE(ClassName) \
-        ClassName() = delete;                             \
-        ~ClassName() = delete;                            \
+    #define IMPLEMENT_CLASS_NOT_CONSTRUCTIBLE(ClassName) \
+        ClassName() = delete;                            \
+        ~ClassName() = delete;                           \
         IMPLEMENT_CONSTRUCTORS_DELETED(ClassName)
 #endif
 
 #ifndef IMPLEMENT_CONSTRUCTORS_DEFAULT
-#    define IMPLEMENT_CONSTRUCTORS_DEFAULT(ClassName) CLASS_CONSTRUCTORS_IMPLEMENTATION(ClassName, default)
+    #define IMPLEMENT_CONSTRUCTORS_DEFAULT(ClassName) CLASS_CONSTRUCTORS_IMPLEMENTATION(ClassName, default)
 #endif
 } // namespace be::internals
